c960ba343069f6280e22174b8c206614266bc870
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-08 16:34:18 -0300

    First test

--- src/test/java/br/com/thecat/TesteApi/TesteApi.java
+++ src/test/java/br/com/thecat/TesteApi/TesteApi.java
@@ -2,10 +2,20 @@ package br.com.thecat.TesteApi;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+
+
 public class TesteApi {
 
 	@Test
 	public void cadastro() {
 		
+		String url = "https://api.thecatapi.com/v1/user/passwordlesssignup";
+		String body = "{\"email\": \"felipeam10@hotmail.com\", \"appDescription\": \"test with rest-assured II\"}";
+		
+		given().contentType("application/json").body(body)
+		.when().post(url)
+		.then().statusCode(200);
 	}
 }
+
