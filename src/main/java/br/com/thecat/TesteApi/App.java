da0e156152eb6ac8c5ec8139eaf80d1ec8132c21
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-08 16:18:01 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/thecat/TesteApi/App.java
@@ -0,0 +1,13 @@
+package br.com.thecat.TesteApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/thecat/TesteApi/TesteApi.java
@@ -0,0 +1,11 @@
+package br.com.thecat.TesteApi;
+
+import org.junit.Test;
+
+public class TesteApi {
+
+	@Test
+	public void cadastro() {
+		
+	}
+}
